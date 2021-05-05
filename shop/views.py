f5880077b5a3852a46baadb0c0fbfae5313f47f4
masoud <masoud_mohebali@yahoo.com>
2021-05-05 16:05:08 +0430

    create cart app

--- shop/views.py
+++ shop/views.py
@@ -17,18 +17,7 @@ def product_detail(request, slug):
     product = get_object_or_404(Product,slug=slug)
     return render(request,'shop/product_detail.html', {'product':product})
 
-# def category(request,slug=None):
-#     categories = Category.objects.all()
-#     if slug:
-#         category = get_object_or_404(Category,slug=slug)
-#         # products = Product.objects.filter(category=category)
-#
-#     return render(request,'inc/navbar.html',{'categories':categories})
 
-
-# def category(request,slug):
-#     categories = get_object_or_404(Category,slug=slug)
-#     return render(request,'shop/category.html',{'categories':categories})
 
 def sub_category(request, slug):
     subcategory = get_object_or_404(SubCategory,slug=slug)
